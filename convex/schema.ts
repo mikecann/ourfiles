1e19fb8640309ed5da5953c9d838ad4a7b6a61e4
Mike Cann <mike.cann@gmail.com>
2025-02-07 07:03:59 +0800

    removed the convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,4 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-// The schema is entirely optional.
-// You can delete this file (schema.ts) and the
-// app will continue to work.
-// The schema provides more precise TypeScript types.
-export default defineSchema({
-  messages: defineTable({
-    author: v.string(),
-    body: v.string(),
-  }),
-});
+export default defineSchema({});
