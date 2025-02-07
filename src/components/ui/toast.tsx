aa3cb61fc0b4a0f39ee9452a403b9eb47c69d2a1
Mike Cann <mike.cann@gmail.com>
2025-02-07 14:17:40 +0800

    nicer color toasts

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -4,11 +4,13 @@ export function Toaster() {
   return (
     <SonnerToaster
       position="bottom-right"
+      className="group"
       toastOptions={{
-        style: {
-          background: "white",
-          color: "black",
-          border: "1px solid #E5E7EB",
+        classNames: {
+          toast: "border bg-white text-black border-gray-200",
+          success: "bg-green-50 border-green-200 text-green-800",
+          loading: "bg-blue-50 border-blue-200 text-blue-800",
+          error: "bg-red-50 border-red-200 text-red-800",
         },
       }}
     />
