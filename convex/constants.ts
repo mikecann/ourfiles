b584de331c839e5f8aa5ffa2ff9bddefd7978d09
Mike Cann <mike.cann@gmail.com>
2025-02-10 12:12:59 +0800

    better

--- convex/constants.ts
+++ convex/constants.ts
@@ -1,12 +1,13 @@
-declare global {
-  interface ImportMetaEnv {
-    CONVEX_MAX_FILE_SIZE_MB?: string;
-  }
-}
-
 // File size limits
 const DEFAULT_MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
-export const MAX_FILE_SIZE = DEFAULT_MAX_FILE_SIZE;
+
+export const MAX_FILE_SIZE =
+  typeof process !== "undefined" && process.env.MAX_FILE_SIZE
+    ? parseInt(process.env.MAX_FILE_SIZE)
+    : DEFAULT_MAX_FILE_SIZE;
 
 // Timeouts
-export const UPLOAD_TIMEOUT_MS = 10000; // 10 seconds
+export const UPLOAD_TIMEOUT_MS =
+  typeof process !== "undefined" && process.env.UPLOAD_TIMEOUT_MS
+    ? parseInt(process.env.UPLOAD_TIMEOUT_MS)
+    : 10000; // 10 seconds
