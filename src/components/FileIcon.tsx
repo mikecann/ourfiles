17362e3e36eae46d1d9a55fb9dc2aeb1c63b5ed5
Mike Cann <mike.cann@gmail.com>
2025-05-09 12:02:36 +0800

    okay this should work

--- src/components/FileIcon.tsx
+++ src/components/FileIcon.tsx
@@ -49,83 +49,98 @@ export const FileIcon: React.FC<FileIconProps> = ({
   // Don't apply transitions if this is a ghost preview (has pointer-events: none)
   const isGhostPreview = style?.pointerEvents === "none";
 
+  const left = file.position.x - 20;
+  const top = file.position.y - 20;
+
   return (
-    <div
-      className="absolute flex flex-col items-center gap-1 cursor-pointer select-none pointer-events-auto file-icon z-10"
-      style={{
-        left: file.position.x - 20, // Center the icon on drop position
-        top: file.position.y - 20,
-        transition:
-          !isGhostPreview && !isDragging && animate
-            ? "left 0.2s ease-out, top 0.2s ease-out"
-            : "none",
-        ...style,
-      }}
-      draggable={draggable}
-      onDragStart={
-        onDragStart
-          ? (e: React.DragEvent) => {
-              setIsDragging(true);
-              onDragStart?.(e);
-            }
-          : undefined
-      }
-      onDrag={onDrag}
-      onDragEnd={
-        onDragEnd
-          ? (e: React.DragEvent) => {
-              setIsDragging(false);
-              onDragEnd?.(e);
-            }
-          : undefined
-      }
-      onTouchStart={onTouchStart}
-      onTouchMove={onTouchMove}
-      onTouchEnd={onTouchEnd}
-    >
-      {!disableTooltip && tooltip}
-      <div className="relative">
-        <div
-          className={`
+    <>
+      <div
+        className="absolute flex flex-col items-center gap-1 cursor-pointer select-none pointer-events-auto file-icon z-10"
+        style={{
+          left,
+          top,
+          transition:
+            !isGhostPreview && !isDragging && animate
+              ? "left 0.2s ease-out, top 0.2s ease-out"
+              : "none",
+          ...style,
+        }}
+        draggable={draggable}
+        onDragStart={
+          onDragStart
+            ? (e: React.DragEvent) => {
+                setIsDragging(true);
+                onDragStart?.(e);
+              }
+            : undefined
+        }
+        onDrag={onDrag}
+        onDragEnd={
+          onDragEnd
+            ? (e: React.DragEvent) => {
+                setIsDragging(false);
+                onDragEnd?.(e);
+              }
+            : undefined
+        }
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
+        <div className="relative">
+          <div
+            className={`
             w-10 h-10 rounded-lg shadow-sm border flex items-center justify-center relative overflow-hidden
             transition-all duration-200
             ${isSelected ? "bg-blue-50 border-blue-400 ring-2 ring-blue-400 ring-opacity-50" : "bg-white hover:bg-gray-50"}
           `}
+            onClick={onClick}
+            onMouseDown={onMouseDown}
+            onDoubleClick={handleDoubleClick}
+          >
+            <FileTypeIcon
+              type={file.type}
+              className={isSelected ? "text-blue-400" : "text-gray-300"}
+            />
+            {(file.uploadState.kind === "uploading" ||
+              file.uploadState.kind === "errored") && (
+              <div
+                className={`absolute bottom-0 left-0 right-0 h-1 ${file.uploadState.kind === "errored" ? "bg-red-100" : "bg-blue-100"}`}
+              >
+                <div
+                  className={`h-full transition-all duration-200 ${file.uploadState.kind === "errored" ? "bg-red-500" : "bg-blue-500"}`}
+                  style={{
+                    width:
+                      file.uploadState.kind === "uploading"
+                        ? `${file.uploadState.progress}%`
+                        : "100%",
+                  }}
+                />
+              </div>
+            )}
+          </div>
+          <UploadStatusIndicator file={file} />
+        </div>
+        <span
+          className={`text-xs max-w-[100px] truncate ${isSelected ? "text-blue-500" : "text-gray-600"}`}
           onClick={onClick}
           onMouseDown={onMouseDown}
           onDoubleClick={handleDoubleClick}
         >
-          <FileTypeIcon
-            type={file.type}
-            className={isSelected ? "text-blue-400" : "text-gray-300"}
-          />
-          {(file.uploadState.kind === "uploading" ||
-            file.uploadState.kind === "errored") && (
-            <div
-              className={`absolute bottom-0 left-0 right-0 h-1 ${file.uploadState.kind === "errored" ? "bg-red-100" : "bg-blue-100"}`}
-            >
-              <div
-                className={`h-full transition-all duration-200 ${file.uploadState.kind === "errored" ? "bg-red-500" : "bg-blue-500"}`}
-                style={{
-                  width:
-                    file.uploadState.kind === "uploading"
-                      ? `${file.uploadState.progress}%`
-                      : "100%",
-                }}
-              />
-            </div>
-          )}
-        </div>
-        <UploadStatusIndicator file={file} />
+          {file.name}
+        </span>
       </div>
-      <span
-        className={`text-xs max-w-[100px] truncate ${isSelected ? "text-blue-500" : "text-gray-600"}`}
-        onClick={onClick}
-        onMouseDown={onMouseDown}
-        onDoubleClick={handleDoubleClick}
-      >
-        {file.name}
-      </span>
-    </div>
+      {!disableTooltip && tooltip && (
+        <div
+          className="absolute flex flex-col items-center gap-1 cursor-pointer select-none pointer-events-auto file-icon z-50"
+          style={{
+            left: left + 52,
+            top,
+          }}
+        >
+          {tooltip}
+        </div>
+      )}
+    </>
   );
 };
